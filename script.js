21bf96106c94afe08078c93d713816b1eccadda7
paiabhishekpai <abhishekupai1124@gmail.com>
2024-09-23 21:32:47 +0530

    songs listed in playlist

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith( ".mp3")){
-            songs.push(element.href);
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
     return songs
@@ -21,6 +21,20 @@ async function main(){
     //get songs
     let songs= await getSongs();
     console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    songUL.innerHTML = ""
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Harry</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div> </li>`;
+    }
 
     //to play the first song
     var audio= new Audio(songs[0]);
