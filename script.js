e2e6ec7909ea680054f1cf1a8ed14abf299905c9
paiabhishekpai <abhishekupai1124@gmail.com>
2024-09-29 00:47:15 +0530

    adjusting volume using range input

--- script.js
+++ script.js
@@ -129,6 +129,11 @@ async function main() {
             playMusic(songs[index+1])
         }
     })
+
+    //adding event listener to change volume
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change",(e)=>{
+        currentSong.volume = parseInt(e.target.value)/100;
+    })
 }
 
 main();
